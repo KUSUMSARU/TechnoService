0ebf45469d9b3db1856d61e0ff2ec5ba71381253
Deep6010 <rejdeep6010@gmail.com>
2021-08-06 14:15:59 +0545

    cart

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,4 @@
 from django.core.exceptions import ObjectDoesNotExist
-from django.http.request import HttpRequest
-from django.http.response import HttpResponse
 from carts.models import CartItem, Cart
 from django.shortcuts import get_object_or_404, redirect, render
 from store.models import Product
@@ -16,9 +14,12 @@ def _cart_id(request):
 def add_cart(request, product_id):
     product = Product.objects.get(id = product_id) # get the product
     try:
-        cart = Cart.objects.get(is_active = True) #get the cart using the cart_id present in the session
+        cart = Cart.objects.get(cart_id = _cart_id(request)) #get the cart using the cart_id present in the session
     except Cart.DoesNotExist:
-        cart = Cart.objects.create()
+        cart = Cart.objects.create(
+            cart_id = _cart_id(request)
+        )
+    cart.save()
 
     try:
         cart_item = CartItem.objects.get(product = product, cart = cart)
@@ -35,8 +36,9 @@ def add_cart(request, product_id):
 
 
 
+
 def remove_cart(request, product_id):
-    cart = Cart.objects.get(is_active = True)
+    cart = Cart.objects.get(cart_id=_cart_id(request))
     product =get_object_or_404(Product, id=product_id)
     cart_item =CartItem.objects.get(product=product, cart=cart)
     if cart_item.quantity > 1:
@@ -48,7 +50,7 @@ def remove_cart(request, product_id):
 
 
 def remove_cart_item(request, product_id):
-    cart = Cart.objects.get(is_active=True)
+    cart = Cart.objects.get(cart_id=_cart_id(request))
     product = get_object_or_404(Product, id=product_id)
     cart_item =CartItem.objects.get(product=product, cart=cart)
     cart_item.delete()
@@ -59,12 +61,12 @@ def remove_cart_item(request, product_id):
 
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
-        cart = Cart.objects.get(is_active = True)
-        cart_items = CartItem.objects.filter(cart=cart)
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active = True)
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity +=cart_item.quantity
-        tax = (2 * total)/100
+        tax = (13 * total)/100
         grand_total = total + tax
     except ObjectDoesNotExist:
         pass #just ignore
